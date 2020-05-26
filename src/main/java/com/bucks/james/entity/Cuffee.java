fde9916366ace8cef9ce7ec2276c30918c769957
ZhanKobe <1625589073@qq.com>
2020-05-27 00:09:12 +0800

    #func01: 添加实体类

---
+++ src/main/java/com/bucks/james/entity/Cuffee.java
@@ -0,0 +1,18 @@
+package com.bucks.james.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: JamesZhan
+ * @create: 2020 - 05 - 27 0:08
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cuffee {
+}
